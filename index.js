f54760df4179b8a357cc55bf042b4421b8dbc315
Luke Bonaccorsi <luke@lukeb.co.uk>
2014-06-29 00:03:41 +0100

    Initial commit for submodule

---
+++ index.js
@@ -0,0 +1,16 @@
+var hangoutsBot = require("hangouts-bot");
+var bot = new hangoutsBot("", "");
+
+module.exports = function(api){
+
+    bot.on('online', function() {
+        api.addMessageSender('hangouts', function(message, to){
+            bot.sendMessage(to, message);
+        });
+    });
+
+    bot.on('message', function(from, message) {
+        api.messageRecieved(from, 'hangouts', message)
+    });
+}
+
