175bebeb7f32bb2f84afdbad139c3d16200e5d01
Luke Bonaccorsi <luke@lukeb.co.uk>
2016-02-13 22:31:16 +0000

    Fixed issue with too many listeners

--- index.js
+++ index.js
@@ -19,74 +19,78 @@ var xmppCore = require('node-xmpp-core'),
     }
 
 hangouts.prototype.init = function(){
-    var self = this;
     this.getPrefs().done(function(prefs){
-        self.connection = connection = new xmppClient({
-            jid: prefs.username,
-            password: prefs.password,
-            host: "talk.google.com",
-            reconnect: true
+        this.prefs = prefs;
+        this.connect();
+    }.bind(this));
+
+}
+
+hangouts.prototype.connect = function() {
+    this.connection = connection = new xmppClient({
+        jid: this.prefs..username,
+        password: this.prefs..password,
+        host: "talk.google.com",
+        reconnect: true
+    });
+
+    this.connection.connection.socket.setTimeout(0)
+    this.connection.connection.socket.setKeepAlive(true, 10000)
+
+    this.connection.on('disconnect', function() {
+        this.connect();
+    }.bind(this))
+
+    this.connection.on('online', function() {
+        connection.send(new xmppCore.Element('presence', {})
+            .c('show')
+            .t('chat')
+            .up()
+            .c('status')
+            .t('Online')
+        );
+
+        var roster_elem = new xmppCore.Element('iq', {
+            'from': connection.jid,
+            'type': 'get',
+            'id': 'google-roster'
+        }).c('query', {
+            'xmlns': 'jabber:iq:roster',
+            'xmlns:gr': 'google:roster',
+            'gr:ext': '2'
         });
 
-        self.connection.connection.socket.setTimeout(0)
-        self.connection.connection.socket.setKeepAlive(true, 10000)
+        connection.send(roster_elem);
 
-        self.connection.on('disconnect', function() {
-            self.connection.connect();
-        })
+        this.addMessageSender(function(message, to){
+                var stanza = new xmppCore.Element('message',
+                    {
+                        to: to,
+                        type: 'chat'
+                    })
+                    .c('body')
+                    .t(message);
 
-        self.connection.on('online', function() {
-            connection.send(new xmppCore.Element('presence', {})
-                .c('show')
-                .t('chat')
-                .up()
-                .c('status')
-                .t('Online')
-            );
-
-            var roster_elem = new xmppCore.Element('iq', {
-                'from': connection.jid,
-                'type': 'get',
-                'id': 'google-roster'
-            }).c('query', {
-                'xmlns': 'jabber:iq:roster',
-                'xmlns:gr': 'google:roster',
-                'gr:ext': '2'
-            });
-
-            connection.send(roster_elem);
-
-            self.addMessageSender(function(message, to){
-                    var stanza = new xmppCore.Element('message',
-                        {
-                            to: to,
-                            type: 'chat'
-                        })
-                        .c('body')
-                        .t(message);
-
-                    self.connection.send(stanza);
-            });
+                this.connection.send(stanza);
         });
+    });
 
 
-        self.connection.on('stanza', function(stanza) {
-            if (stanza.is('message') && (stanza.attrs.type !== 'error') && (stanza.getChildText('body'))) {
-                var userParts = stanza.attrs.from.split('/');
+    this.connection.on('stanza', function(stanza) {
+        if (stanza.is('message') && (stanza.attrs.type !== 'error') && (stanza.getChildText('body'))) {
+            var userParts = stanza.attrs.from.split('/');
 
-                userParts = userParts.slice(0, userParts.length - 1);
-                self.messageRecieved(stanza.attrs.from, stanza.getChildText('body'), userParts.join('/'));
-            }
+            userParts = userParts.slice(0, userParts.length - 1);
+            this.messageRecieved(stanza.attrs.from, stanza.getChildText('body'), userParts.join('/'));
+        }
 
-            if(stanza.is('presence') && stanza.attrs.type === 'subscribe') {
-                stanza.attrs.to = stanza.attrs.from;
-                delete stanza.attrs.from;
+        if(stanza.is('presence') && stanza.attrs.type === 'subscribe') {
+            stanza.attrs.to = stanza.attrs.from;
+            delete stanza.attrs.from;
 
-                connection.send(stanza);
-            }
-        });
-    })
-
+            connection.send(stanza);
+        }
+    });
 }
 
 hangouts.prototype.exit = function(){
